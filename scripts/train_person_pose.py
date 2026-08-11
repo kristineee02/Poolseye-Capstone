915ac95354cb73a11e4cc930ae80443a9b9b23d1
kristineee02 <kristinesabuero249@gmail.com>
2026-08-11 20:36:24 +0800

    person detection

--- scripts/train_person_pose.py
+++ scripts/train_person_pose.py
@@ -3,14 +3,13 @@ from ultralytics import YOLO
 def main():
     model = YOLO("yolo11n-pose.pt")
     
-    # Ultralytics to download and set up the required portions of COCO.
     results = model.train(
-        data="coco-pose.yaml",
+        data="dataset/person_pose/dataset.yaml",
         epochs=50,
         imgsz=640,
         batch=16,
-        device=0,
-        name="pool_person_pose_coco"
+        device="cpu",  # <--- Change this from 0 to "cpu"
+        name="pool_person_pose_run"
     )
 
 if __name__ == '__main__':
